5661b8c467b7cab52dd5849acba7e3e0b2d1b6cd
JaaJPlayz <hiagoaugustofcunha@gmail.com>
2024-11-08 12:15:28 -0300

    Ta-da: Setting up a database

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello world!')
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/api/v1/hello", (_req, res) => {
+    res.json({ message: "Hello world!" });
+});
+
+app.listen(process.env.BACKEND_PORT, () => {
+    console.log(`Server started on port ${process.env.BACKEND_PORT}`);
+});
